d6298d91ec90444b83ecc904433fe5a76ea77365
nesmanpro <nesmanpro@gmail.com>
2024-01-22 00:48:41 +0100

    chat rodando

--- src/app.js
+++ src/app.js
@@ -21,3 +21,31 @@ app.use('/', viewsRouter);
 const httpServer = app.listen(PORT, () => {
     console.log('Escuchando en puerto http://localhost:8080')
 })
+
+
+// socket.io
+
+// Creamos instancia de socket.io
+
+const io = new socket.Server(httpServer);
+
+// Vamos a crear un array que guarde los mensajes de los participantes
+
+const message = [];
+
+
+// Estanlecemos la coneccion:
+// io es la instancia de socket.io
+//'on' es el metodo para escuchar eventos
+// el primer parametro es el evento que queremos escuchar
+//el segundo es un callback, q se ejecuta cuando se ejecuta el evento (cuando se ejecuta la conexion)
+
+io.on('connection', (socket) => {
+    console.log('Nuevo usuario conectado!');
+
+    socket.on('message', data => {
+        message.push(data);
+        io.emit('messageLogs', message)
+        //Con emit, enviamos mensaje desde el servidor al cliente
+    })
+})
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,1 +1,51 @@
-console.log('Estamos ready!')
+// Instancia de socket.io del lado del cliente
+const socket = io();
+
+// variable para guardar el usuario
+
+let user;
+
+const chatBox = document.getElementById('chatBox');
+
+
+//sweet alert 2 
+//Swal es un objeto global que nos permite usar los metodos de la libreria.
+//Fire , es un metodo que nos permite configurar el alert.
+
+
+Swal.fire({
+    title: 'Identificate',
+    input: 'text',
+    text: 'Ingresa un usuario para identificarte',
+    inputValidator: (value) => {
+        return !value && 'necesitas escribir un nombre para continuar'
+    },
+    allowOutsideClick: false,
+}).then(res => {
+    user = res.value;
+    console.log(user)
+})
+
+chatBox.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        if (chatBox.value.trim().length > 0) {
+            // Trim nos permite sacar espacios en blanco del principio y final
+            // Si el mensaje tiene mas de 0 caracteres lo enviamos al servidor
+            socket.emit('message', { user: user, message: chatBox.value });
+            chatBox.value = '';
+
+        }
+    }
+})
+
+// Listener de mensajes
+socket.on('messageLogs', (data) => {
+    let log = document.getElementById('messageLogs');
+    let messages = '';
+
+    data.forEach(message => {
+        messages = messages + `<strong>${message.user} dice:</strong> ${message.message}<br>`
+    });
+
+    log.innerHTML = messages;
+})
